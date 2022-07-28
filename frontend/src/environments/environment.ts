8738c60a125cdaff14b7e944a61e541a8e9efc0c
mr-icon <emmanuelaustin116@gamil.com>
2022-07-27 22:22:59 -0700

    pushing 2nd update

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-3bnjl6tn.us', // the auth0 domain prefix
+    audience: 'coffeeAPI', // the audience set for the auth0 app
+    clientId: 'yJRpChF1hS16JUFXv3Sjfukde7E0YvbF', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
